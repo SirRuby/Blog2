8f4680a1fd25805508da2b08821b9bd33eabbb7d
SirRuby <staffordharrison@protonmail.com>
2020-06-14 00:02:43 +0700

    Finished carrierwave installation

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,9 +1,15 @@
-def portfolio_img img, type
-  if !img.nil?
-    img
-  elsif type == 'thumb'
-    image_generator(height: '350', width: '200')
-  elsif 
-    image_generator(height: '600', width: '400')
+module PortfoliosHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
   end
-end
+
+  def portfolio_img img, type
+    if !img.nil?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    elsif 
+      image_generator(height: '600', width: '400')
+    end
+  end 
+end 
